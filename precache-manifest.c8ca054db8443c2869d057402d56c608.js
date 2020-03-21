59bf1025212d90395d230105781baa399a2d4d4a
Colm McSharry <colmmcsharry@hotmail.com>
2020-03-21 01:08:10 -0700

    Updates

---
+++ precache-manifest.c8ca054db8443c2869d057402d56c608.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23b6bdd99bf3e68eab807cf1f700461",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "79b68e933ed876afa461",
+    "url": "/my-react-app/static/css/main.7a16929d.chunk.css"
+  },
+  {
+    "revision": "683c14deb9f1ba70fc59",
+    "url": "/my-react-app/static/js/2.8b451e7b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/my-react-app/static/js/2.8b451e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b68e933ed876afa461",
+    "url": "/my-react-app/static/js/main.c80f204c.chunk.js"
+  },
+  {
+    "revision": "48acf9ca3b29ac4e865e",
+    "url": "/my-react-app/static/js/runtime-main.8897f3f6.js"
+  },
+  {
+    "revision": "8ecb94346c6252880ac0bde2ca969c68",
+    "url": "/my-react-app/static/media/br.8ecb9434.png"
+  },
+  {
+    "revision": "3b89dffd406f2bad641754fca9c19386",
+    "url": "/my-react-app/static/media/dog.3b89dffd.JPG"
+  },
+  {
+    "revision": "7a30437d95b3ac59b8597a9241e7f39d",
+    "url": "/my-react-app/static/media/networkerspic.7a30437d.png"
+  },
+  {
+    "revision": "56c38101b38f20ee2bf24f8134eb441e",
+    "url": "/my-react-app/static/media/networkerspic2.56c38101.png"
+  },
+  {
+    "revision": "af1220ebceacc61a10a8231ea1cc005c",
+    "url": "/my-react-app/static/media/networkerspic3.af1220eb.png"
+  },
+  {
+    "revision": "7b657b0f1777a8df12f03c8eeb93f553",
+    "url": "/my-react-app/static/media/networkersshot.7b657b0f.png"
+  },
+  {
+    "revision": "fe4d52f6c963564923ebbba3d0627b34",
+    "url": "/my-react-app/static/media/panb.fe4d52f6.png"
+  },
+  {
+    "revision": "6498610a56acd68ae3206bf4ec9dbe92",
+    "url": "/my-react-app/static/media/pand.6498610a.png"
+  },
+  {
+    "revision": "aaa4b8a5d5dc1b3d58f7aec84896d715",
+    "url": "/my-react-app/static/media/smileysun.aaa4b8a5.png"
+  },
+  {
+    "revision": "630105e5b87a955585ad4a782f412741",
+    "url": "/my-react-app/static/media/stick.630105e5.JPG"
+  }
+]);
